1f87bd8d6f5b1acbb4ff655a5f31fc087e34fb4b
unknown <timonpoo220@gmail.com>
2024-07-01 04:25:34 -0700

    feat: build startup template

---
+++ App.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Navigation from './src/routes/Navigation';
+
+export default function App() {
+  return <Navigation />;
+}
+
+const styles = StyleSheet.create({});
